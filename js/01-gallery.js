21428be7d21c27efc0b9f3bcc8ad12103b590ef0
PiLoT-P <59173880+PiLoT-P@users.noreply.github.com>
2023-02-24 16:47:56 +0200

    обнова

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const icon = galleryItems
+    .map(image => 
+        `<div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                class="gallery__image"
+                src="${image.preview}"
+                data-source="${image.original}"
+                alt="${image.description}"
+                />
+            </a>
+        </div>`)
+    .join('');
+gallery.insertAdjacentHTML('afterbegin', icon);
+
+function bigImage(event) {
+    event.preventDefault();
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`)
+    instance.show()
+}
+
+gallery.addEventListener('click', bigImage);
